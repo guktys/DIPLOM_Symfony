8822555824e21af189b38219d538febdf66083c6
Evgenia Deken <guk.genia@gmail.com>
2024-04-07 23:01:32 +0300

    Добавила в табики выбор фото

--- src/Controller/UserCabinetController.php
+++ src/Controller/UserCabinetController.php
@@ -2,24 +2,37 @@
 
 namespace App\Controller;
 
+use App\Repository\AppointmentRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserCabinetController extends AbstractController
 {
+    private UserRepository $userRepository;
+    private AppointmentRepository $appointmentRepository;
+    public function __construct(UserRepository $userRepository,AppointmentRepository $appointmentRepository)
+    {
+        $this->userRepository = $userRepository;
+        $this->appointmentRepository = $appointmentRepository;
+    }
     #[Route('/master_cabinet', name: 'master_cabinet')]
     public function master_cabinet()
     {
 
         return $this->render('master_cabinet.html.twig',[
+
         ]);
     }
 
     #[Route('/user_cabinet', name: 'user_cabinet')]
     public function user_cabinet()
     {
-
+        $user = $this->getUser();
+        $userAppointments = $this->appointmentRepository->findBy(['user'=>$this->getUser()]);
         return $this->render('user_cabinet.html.twig',[
+            'user' => $user,
+            'userAppointments'=>$userAppointments,
         ]);
     }
 }
--- src/Repository/AppointmentRepository.php
+++ src/Repository/AppointmentRepository.php
@@ -2,9 +2,15 @@
 
 namespace App\Repository;
 
+use App\Entity\Appointment;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
-class AppointmentRepository extends EntityRepository
+class AppointmentRepository extends ServiceEntityRepository
 {
-
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Appointment::class);
+    }
 }
